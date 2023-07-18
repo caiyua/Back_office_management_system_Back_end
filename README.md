bf2df0984728ed835b3c3732912373b4d1dba19d
xiaocaiyuan <3043211906@qq.com>
2023-07-18 17:14:53 +0800

    The-End

---
+++ README.md
@@ -0,0 +1,3 @@
+https://apifox.com/apidoc/shared-8563035d-fe01-4860-8d23-d27c900d6d22
+
+打开网址即可查看接口文档
--- config/constant.js
+++ config/constant.js
@@ -1,5 +1,5 @@
 const USER_AVATAR = 'public/upload/user_avatar'
-const PORT = 3000
+const PORT = 80
 const SERVER_URL = 'http://localhost:'
 
 module.exports = { USER_AVATAR, PORT, SERVER_URL }
