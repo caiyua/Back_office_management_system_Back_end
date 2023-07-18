5c9c70674bc124ca02333a528cc44b6302c5f3b2
xiaocaiyuan <3043211906@qq.com>
2023-07-18 21:36:51 +0800

    The-End

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -36,14 +36,19 @@ exports.queryPunchRecords = (req, res) => {
 			return res.status(500).json({ status: 500, message: err.message })
 		}
 		if (results.length < 1) {
-			return res.status(204).json({ status: 204 })
+			return res.status(204).json({ status: 204, message: '暂无打卡信息' })
 		}
 		for (let item of results) {
 			delete item.id
 			delete item.user_id
 			// 转换为北京时间
-			const date = new Date(item.date)
-			item.date = date.toLocaleString('zh-CN', {
+			// 假设item.date是一个字符串类型的时间，格式为：2023/7/18 13:19:31
+			// 将字符串时间转换成Date对象
+			let originalDate = new Date(item.date)
+			// 增加8个小时
+			originalDate.setHours(originalDate.getHours() + 8)
+			// 将结果转换为北京时间格式字符串：yyyy/MM/dd HH:mm:ss
+			item.date = originalDate.toLocaleString('zh-CN', {
 				timeZone: 'Asia/Shanghai',
 			})
 		}
@@ -57,19 +62,21 @@ exports.queryPunchRecords = (req, res) => {
 exports.addPunchRecord = (req, res) => {
 	let { id, punch_in, reason } = req.query
 	console.log(id, punch_in)
+
 	/*
 	 * 判空
 	 * */
-	if (id === undefined) return res.status(400).json({ status: 400, message: '请传入正确参数（id）' })
+	if (!id) return res.status(400).json({ status: 400, message: '请传入正确参数（id）' })
 	if (punch_in === undefined) return res.status(400).json({ status: 400, message: '请传入正确参数（punch_in）' })
 
 	/*
 	 * 转换类型
 	 * */
-	if (typeof id !== 'number') id = parseFloat(id)
-	if (typeof punch_in !== 'number') punch_in = parseFloat(punch_in)
-	if (typeof reason === 'undefined') reason = ''
-	if (typeof reason !== 'string') reason = reason.toString()
+	id = parseInt(id)
+	punch_in = parseInt(punch_in)
+
+	if (reason === undefined) reason = ''
+	reason = String(reason)
 
 	// 打卡成功 || 传原因
 	if (punch_in === 1 && reason.length > 0) {
@@ -79,41 +86,29 @@ exports.addPunchRecord = (req, res) => {
 		})
 	}
 
-	// 获取当前时间
-	const currentTime = new Date()
-	// 使用本地时间
-	const localTime = currentTime.toLocaleString()
-	// 打卡失败
-	if (punch_in === 0) {
-		console.log('打卡失败')
-		const addPunchRecordSql = 'insert into punch_records (user_id, punch_in, reason, date) value (?,?,?,?)'
-		db.query(addPunchRecordSql, [id, punch_in, reason, localTime], (err, results) => {
-			if (err) {
-				return res.status(500).json({ status: 500, message: err.message })
-			}
+	// 打卡失败或打卡成功
+	const addPunchRecordSql = 'INSERT INTO punch_records (user_id, punch_in, reason) VALUES (?,?,?)'
+	db.query(addPunchRecordSql, [id, punch_in, reason], (err, results) => {
+		if (err) {
+			return res.status(500).json({ status: 500, message: err.message })
+		}
+
+		if (punch_in === 0) {
+			console.log('打卡失败')
 			return res.status(200).json({
 				status: 200,
 				message: '未打卡',
 				data: { results },
 			})
-		})
-		return
-	}
-	// 打卡成功
-	if (punch_in === 1) {
-		const addPunchRecordSql = 'insert into punch_records (user_id, punch_in, date) value (?,?, ?)'
-		db.query(addPunchRecordSql, [id, punch_in, localTime], (err, results) => {
-			if (err) {
-				return res.status(500).json({ status: 500, message: err.message })
-			}
+		} else if (punch_in === 1) {
 			console.log('打卡成功')
 			return res.status(200).json({
 				status: 200,
 				message: '打卡成功',
 				data: { results },
 			})
-		})
-	}
+		}
+	})
 }
 
 /*
@@ -134,12 +129,7 @@ exports.addUser = (req, res) => {
 	 * 类型转换
 	 * */
 	if (typeof cellPhone !== 'number') cellPhone = Number(cellPhone)
-	if (
-		typeof username !== 'string' ||
-		typeof cellPhone !== 'number' ||
-		typeof role !== 'string' ||
-		typeof onBoardTime !== 'string'
-	) {
+	if (typeof username !== 'string' || typeof cellPhone !== 'number' || typeof role !== 'string' || typeof onBoardTime !== 'string') {
 		return res.status(400).json({ status: 400, message: '必要值类型错误！' })
 	}
 
